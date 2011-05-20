ab7fa7a060ddefeafd97a58bb8d3ead4be741ce3
Martin Pöhlmann <martin.deimos@gmx.de>
2011-05-20 12:17:23 +0200

    error handling for resourcs

--- res/resources/R.java
+++ res/resources/R.java
@@ -3,6 +3,8 @@ package resources;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import com.mpdeimos.tensor.util.Log;
+
 /**
  * Class for global resource access.
  * 
@@ -10,22 +12,69 @@ import java.util.ResourceBundle;
  *
  */
 public class R {
-	/** resource bundle for strings */
-	public static final ResourceBundle strings = ResourceBundle.getBundle("strings.strings"); //$NON-NLS-1$
+	/** log tag */
+	private static final String LOG_TAG = "R" ; //$NON-NLS-1$
+	
+	/** string resource provider */
+	public static final StringResourceProvider strings = new StringResourceProvider();
+	
 	/** image resource provider */
-	public static final ResourceProvider drawable = new ResourceProvider();
+	public static final DrawableResourceProvider drawable = new DrawableResourceProvider();
 	
 	/**
-	 * Provides image resolution
+	 * Provides images
 	 */
-	public static class ResourceProvider
+	public static class DrawableResourceProvider
 	{
 		/**
 		 * @return the URL for the given image. prepends "/drawable/", appends ".png".
 		 */
 		public URL getURL(String name)
 		{
-			return R.class.getResource(String.format("/drawable/%s.png", name)); //$NON-NLS-1$
+			URL url = R.class.getResource(String.format("/drawable/%s.png", name)); //$NON-NLS-1$
+			if (url == null)
+			{
+				Log.w(LOG_TAG, "Drawable not found: " + name); //$NON-NLS-1$
+				url = R.class.getResource(String.format("/drawable/default.png", name)); //$NON-NLS-1$
+			}
+			
+			return url;
+		}
+	}
+	
+	/**
+	 * provides string resources
+	 */
+	public static class StringResourceProvider
+	{
+		/** default string if resource not found */
+		private static final String NOT_FOUND = "## ERROR ##" ; //$NON-NLS-1$
+		
+		/** resource bundle for strings */
+		public static final ResourceBundle resourceBundle = ResourceBundle.getBundle("strings.strings"); //$NON-NLS-1$
+
+		/** @return the string for a given key */
+		public String getString(String key)
+		{
+			String str = null;
+			try
+			{
+				str = resourceBundle.getString(key);
+			}
+			catch (Exception e)
+			{
+				// Swallow, will be handled in finally
+			}
+			finally
+			{
+				if (str == null)
+				{
+					Log.w(LOG_TAG, "String not found: " + key); //$NON-NLS-1$
+					str = NOT_FOUND;
+				}
+			}
+			
+			return str;
 		}
 	}
 }
