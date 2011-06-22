49011226421ed0edaaaa0205f41b35599ccf81e7
Martin Pöhlmann <martin.deimos@gmx.de>
2011-06-22 11:43:00 +0200

    interface for roatatable editparts

---
+++ src/com/mpdeimos/tensor/editpart/IRotatableEditPart.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.mpdeimos.tensor.editpart;
+
+
+/**
+ * Base interface for rotatable EditParts.
+ * 
+ * @author mpdeimos
+ *
+ */
+public interface IRotatableEditPart {
+	/** Returns the current rotation of the EditPart in degrees. */
+	public double getRotation();
+	
+	/** Sets the rotation of the current EditPart in degrees. */
+	public void setPosition(double degrees);
+}
