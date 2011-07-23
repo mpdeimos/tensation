2350e7e9899237933bc88f3974ec6ca7b2bf3e5b
Martin Pöhlmann <martin.deimos@gmx.de>
2011-07-23 10:38:11 +0200

    tensor base class

---
+++ src/com/mpdeimos/tensor/model/TensorBase.java
@@ -0,0 +1,84 @@
+package com.mpdeimos.tensor.model;
+
+import com.mpdeimos.tensor.util.ImmutableList;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * abstract base class for tensors.
+ * 
+ * @author mpdeimos
+ * 
+ */
+public abstract class TensorBase extends ModelDataBase
+{
+	/** Position of the object in screen coordinates. */
+	protected final Point position;
+
+	/** Rotation of the current figure */
+	private double rotation = 0;
+
+	/** Anchors for this tensor. */
+	protected final List<TensorConnectionAnchor> anchors = new ArrayList<TensorConnectionAnchor>();
+
+	/** Constructor. */
+	public TensorBase(IModelData parent)
+	{
+		super(parent);
+
+		this.position = new Point(0, 0);
+	}
+
+	/**
+	 * @return the position of this object.
+	 */
+	public Point getPosition()
+	{
+		return this.position;
+	}
+
+	/**
+	 * @return the rotation of this figure in degrees
+	 */
+	public double getRotation()
+	{
+		return this.rotation;
+	}
+
+	/** Sets the position of the current point. */
+	public void setPosition(Point p)
+	{
+		this.position.setLocation(p);
+		fireOnModelDataChanged(this);
+	}
+
+	/**
+	 * Sets the rotation of the current point in degrees.
+	 */
+	public void setRotation(double rotation)
+	{
+		this.rotation = rotation;
+		fireOnModelDataChanged(this);
+	}
+
+	/** @return the anchors of this tensor. */
+	public ImmutableList<TensorConnectionAnchor> getAnchors()
+	{
+		return new ImmutableList<TensorConnectionAnchor>(this.anchors);
+	}
+
+	@Override
+	public boolean remove()
+	{
+		for (TensorConnectionAnchor anchor : this.anchors)
+		{
+			anchor.doOnTensorRemoved();
+		}
+		return super.remove();
+	}
+
+	/** Duplicates this tensor with the given model as parent. */
+	abstract public TensorBase duplicate(IModelData root);
+}
